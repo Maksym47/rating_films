e4528b199bd8545143be48d908d29cd5972fb186
Masym <maks07m@gmail.com>
2022-12-07 00:15:24 +0200

    practice part 4 is done2

--- script.js
+++ script.js
@@ -16,85 +16,66 @@
 
 'use strict';
 
-let numberOfFilms;
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: true,
-};
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-    while (numberOfFilms == 0 || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-    }
-}
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = +prompt('На сколько оцените его?', '');
-        if (a && b || b == 0 && a.length <= 50) {
-            personalMovieDB.movies[a] = b;
+    start() {
+        personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
+        while (personalMovieDB.count == 0 || isNaN(personalMovieDB.count)) {
+            personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+        }
+    },
+    rememberMyFilms() {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt('Один из последних просмотренных фильмов?', ''),
+                b = +prompt('На сколько оцените его?', '');
+            if (a && b || b == 0 && a.length <= 50) {
+                personalMovieDB.movies[a] = b;
+            } else {
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel() {
+        if (personalMovieDB.count < 10) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            alert("Вы классический зритель");
+        } else if (personalMovieDB.count > 30) {
+            alert("Вы киноман");
         } else {
-            i--;
+            alert("Произошла ошибка");
         }
-    }
-}
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        alert("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-        alert("Вы классический зритель");
-    } else if (personalMovieDB.count > 30) {
-        alert("Вы киноман");
-    } else {
-        alert("Произошла ошибка");
-    }
-}
-
-function showMyDB() {
-    if (personalMovieDB.privat === false) {
-    console.log(personalMovieDB);
-    }
-}
-// 3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
-// Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
-// при помощи метода forEach вывести в консоль сообщения в таком виде:
-// "Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
-
-function writeYourGenres() {
-    for (let i = 1; i <= 3; i++) {
-        let ans = prompt(`Ваш любимый жанр под номером ${i}`, '');
-        if (ans === null || ans.length < 1) {
-            i--;
-            continue;
+    },
+    showMyDB() {
+        if (personalMovieDB.privat === false) {
+        console.log(personalMovieDB);
         }
-        personalMovieDB.genres.push(ans);
+    },
+    writeYourGenres() {
+        for (let i = 1; i <= 3; i++) {
+            let ans = prompt(`Ваш любимый жанр под номером ${i}`, '');
+            if (ans === null || ans.length < 1) {
+                i--;
+            }
+            personalMovieDB.genres.push(ans);
+        }
+    
+        personalMovieDB.genres.forEach((item, index) => {
+            console.log(`Любимый жанр #${index} - это ${item}`);
+        });
+    },
+    toggleVisibleMyDB() {
+        if (personalMovieDB.privat === true) {
+            personalMovieDB.privat = false;
+        } else if (personalMovieDB.privat === false) {
+            personalMovieDB.privat = true;
+        } 
     }
-
-    personalMovieDB.genres.forEach((item) => {
-        console.log(item);
-    });
-}
-
-function toggleVisibleMyDB() {
-    if (personalMovieDB.privat === true) {
-        personalMovieDB.privat = false;
-    } else if (personalMovieDB.privat === false) {
-        personalMovieDB.privat = true;
-    } 
-}
-
-personalMovieDB.toggleVisibleMyDB = toggleVisibleMyDB;
-personalMovieDB.start = start;
-personalMovieDB.rememberMyFilms = rememberMyFilms;
-personalMovieDB.detectPersonalLevel = detectPersonalLevel;
-personalMovieDB.showMyDB = showMyDB;
-personalMovieDB.writeYourGenres = writeYourGenres;
+};
 
 personalMovieDB.writeYourGenres();
